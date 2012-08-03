92e7d6ebbc7390c6d7bc9560cdd937ead5f9feb9
Marcin Gryszko <marcin@grysz.com>
2012-08-03 13:09:56 +0200

    OpenJDK installed

--- manifests/ubuntu-12.04.pp
+++ manifests/ubuntu-12.04.pp
@@ -1,11 +1,12 @@
-class git {
-  exec { 'apt-get update':
-    command => '/usr/bin/apt-get update'
-  }
-
-  package { "git":
-    ensure => present,
-  }
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get update'
 }
 
-include git
+package { "git":
+  ensure => present,
+}
+
+package { "openjdk-6-jdk":
+  require => Exec['apt-get update'],
+  ensure => present,
+}
