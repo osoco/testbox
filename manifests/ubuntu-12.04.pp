4d073678d84f49d5b8267a85d270b24590110c2b
David Molinero <davidmolinero.com@gmail.com>
2012-08-03 15:32:23 +0200

    Git with repository cloning

--- manifests/ubuntu-12.04.pp
+++ manifests/ubuntu-12.04.pp
@@ -1,12 +1,20 @@
-exec { 'apt-get update':
-  command => '/usr/bin/apt-get update'
+class git_core { 
+	exec { 'apt-get update':
+		command => '/usr/bin/apt-get update'
+	}
+
+    package { 'git':
+        ensure => present,
+    }
 }
 
-package { "git":
-  ensure => present,
+define git::clone($path) {
+	include git_core
+    exec { 'git clone':
+    	command => "/usr/bin/git clone $path /home/vagrant/RMB-BACKOFFICE-BMS"
+    }
 }
 
-package { "openjdk-6-jdk":
-  require => Exec['apt-get update'],
-  ensure => present,
+git::clone { 'git clone repo':
+	path => '/home/dmcom/Documents/osoco/rumbo/bms/RMB-BACKOFFICE-BMS',
 }
