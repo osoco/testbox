1628e236bd60411ba33731000db7c8428d23ca55
Marcin Gryszko <marcin@grysz.com>
2012-08-10 09:29:39 +0200

    Basic development environment installation

--- manifests/ubuntu-12.04.pp
+++ manifests/ubuntu-12.04.pp
@@ -1,20 +1,56 @@
-class git_core { 
-	exec { 'apt-get update':
-		command => '/usr/bin/apt-get update'
-	}
+stage { 'init': before => Stage['main'] }
 
-    package { 'git':
+class {
+  'init': stage => 'init',
+}  
+
+class init {
+  exec { 'initial-apt-get-update':
+    command => '/usr/bin/apt-get update',
+  }
+
+  package { 'python-software-properties':
+    ensure => present,
+  }
+  
+  Exec['initial-apt-get-update'] -> Package['python-software-properties']
+}
+
+class git_core { 
+	package { 'git':
         ensure => present,
     }
 }
 
 define git::clone($path) {
-	include git_core
+    include git_core
     exec { 'git clone':
-    	command => "/usr/bin/git clone $path /home/vagrant/RMB-BACKOFFICE-BMS"
+        command => "/usr/bin/git clone $path /home/vagrant/RMB-BACKOFFICE-BMS"
     }
 }
 
 git::clone { 'git clone repo':
 	path => '/home/dmcom/Documents/osoco/rumbo/bms/RMB-BACKOFFICE-BMS',
+}
+
+class jdk {
+  package { "openjdk-6-jdk":
+    ensure => present,
+  }
+}
+
+class grails {
+  exec { 'ppa:groovy-dev/grails':
+    command => '/usr/bin/add-apt-repository ppa:groovy-dev/grails',
+  }
+
+  exec { 'apt-get-update':
+    command => '/usr/bin/apt-get update',
+}
+
+  package { 'grails-1.3.7':
+    ensure => present,
+  }
+
+  Exec['ppa:groovy-dev/grails'] -> Exec['apt-get-update'] -> Package['grails-1.3.7']
 }
