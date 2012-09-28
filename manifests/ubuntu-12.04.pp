ec816a8a134285a3bcabc22165175aeef0666448
Marcin Gryszko <marcin@grysz.com>
2012-09-28 09:57:35 +0200

    Goldberg replaced with Jenkins

--- manifests/ubuntu-12.04.pp
+++ manifests/ubuntu-12.04.pp
@@ -1,9 +1,7 @@
 Exec { path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'] }
 
-include rvm
-
 stage { 
-  'init': before => Stage['rvm-install', 'main'];
+  'init': before => Stage['main'];
 }
 
 class {
@@ -11,7 +9,7 @@ class {
   'jdk': stage => 'main';
   'grails': stage => 'main';
   'git_core': stage => 'main';
-  'goldberg': stage => 'main';
+  'jenkins': stage => 'main';
 }  
 
 class init {
@@ -42,34 +40,52 @@ class jdk {
 class grails {
   exec { 'ppa:groovy-dev/grails':
     command => 'add-apt-repository ppa:groovy-dev/grails',
+    unless => 'ls /etc/apt/sources.list.d/groovy-dev-grails-*',
   }
 
-  exec { 'apt-get-update':
+  exec { 'grails-apt-get-update':
     command => 'apt-get update',
+    subscribe => Exec['ppa:groovy-dev/grails'],
+    refreshonly => true,
   }
 
   package { 'grails-1.3.7':
     ensure => present,
   }
 
-  Exec['ppa:groovy-dev/grails'] -> Exec['apt-get-update'] -> Package['grails-1.3.7']
+  Exec['grails-apt-get-update'] -> Package['grails-1.3.7']
 }
 
-class goldberg {
-  exec { 'goldberg-clone-repo':
-    command => 'git clone git://github.com/c42/goldberg.git /home/vagrant/goldberg',
-    creates => '/home/vagrant/goldberg',
-    user => 'vagrant',
+class jenkins {
+  package { 'wget': 
+    ensure => present,
   }
 
-  rvm_system_ruby { 'ruby-1.9.3-p194':
-    ensure => 'present',
-    default_use => false;
-  }                      
+  package { 'sed': 
+    ensure => present,
+  }
 
-  rvm_gem { 'bundler':
-    ensure => latest,
-    ruby_version => 'ruby-1.9.3-p194',
-    require => Rvm_system_ruby['ruby-1.9.3-p194'];
+  package { 'tomcat6':
+    ensure => present,
+    require => Package['openjdk-6-jdk'],
+  }
+
+  service { 'tomcat6':
+    ensure => running,
+    require => Package['tomcat6'],
+  }
+
+  exec { 'jenkins-latest-war':
+    command => '/usr/bin/wget --output-document=/var/lib/tomcat6/webapps/jenkins.war http://mirrors.jenkins-ci.org/war/latest/jenkins.war',
+    require => Package['tomcat6'],
+    creates => '/var/lib/tomcat6/webapps/jenkins.war',
+    timeout => 600,
+  }
+
+  exec { 'non-conflicting-tomcat-port':
+    command => "sed -i -e 's/8080/8888/g' /etc/tomcat6/server.xml",
+    notify => Service['tomcat6'],
+    unless => 'grep 8888 /etc/tomcat6/server.xml',
   }
 }
+
