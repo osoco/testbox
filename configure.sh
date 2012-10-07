d87a3a033ccbba792c2f8f299052160eacd6d08f
Marcin Gryszko <marcin@grysz.com>
2012-10-07 09:29:05 +0200

    Obsolete file removed

--- configure.sh
+++
@@ -1,5 +0,0 @@
-template_file=manifests/ubuntu-12.04-template.pp
-outputfile=manifests/ubuntu-12.04.pp
-echo 'Enter the path to your local repository:'
-read path
-sed "s@#path#@$path@g" $template_file > $outputfile
